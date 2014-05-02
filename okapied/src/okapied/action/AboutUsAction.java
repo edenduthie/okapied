13f0322a1d8f3d68fa6f83e3ee62849e7dac4e10
edenduthie <eduthie@gmail.com>
2014-05-02 00:49:40 +0000

    AboutUsAction.java

---
+++ okapied/src/okapied/action/AboutUsAction.java
@@ -0,0 +1,20 @@
+package okapied.action;
+
+
+public class AboutUsAction extends BaseAction
+{
+    public String list()
+    {
+    	return LIST;
+    }
+    
+    public String getTitle()
+    {
+    	return "Okapied Holiday Accommodation - About Us";
+    }
+    
+    public String getDescription()
+    {
+    	return "At Okapied our goal is to make staying in holiday accommodation easier.";
+    }
+}
