67b0d839e63953a6b3df8c57876dd3fd7767e373
edenduthie <eduthie@gmail.com>
2014-05-02 00:50:28 +0000

    TermsAction.java

---
+++ okapied/src/okapied/action/TermsAction.java
@@ -0,0 +1,19 @@
+package okapied.action;
+
+public class TermsAction extends BaseAction
+{
+    public String list()
+    {
+    	return LIST;
+    }
+    
+    public String getTitle()
+    {
+    	return "Okapied Terms and Conditions";
+    }
+    
+    public String getDescription()
+    {
+    	return "Terms and Conditions of use of okapied.com for registered users";
+    }
+}
