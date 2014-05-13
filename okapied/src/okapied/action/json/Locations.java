023ff7850fb5c3cd6a1621da736f0e1489f96db7
edenduthie <eduthie@gmail.com>
2014-05-13 01:12:00 +0000

    Locations.java

---
+++ okapied/src/okapied/action/json/Locations.java
@@ -0,0 +1,54 @@
+package okapied.action.json;
+
+import java.util.List;
+
+import okapied.entity.Place;
+import okapied.service.PlaceService;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Locations extends ActionSupport
+{
+	static String LIST = "list";
+	
+    String searchString;
+    List<Place> places;
+    
+    PlaceService placeService;
+    
+    public void nullServices()
+    {
+    	placeService = null;
+    }
+    
+    public String list()
+    {
+    	places = placeService.searchLocations(searchString);
+    	nullServices();
+    	return LIST;
+    }
+
+	public String getSearchString() {
+		return searchString;
+	}
+
+	public void setSearchString(String searchString) {
+		this.searchString = searchString;
+	}
+
+	public List<Place> getPlaces() {
+		return places;
+	}
+
+	public void setPlaces(List<Place> places) {
+		this.places = places;
+	}
+
+	public PlaceService getPlaceService() {
+		return placeService;
+	}
+
+	public void setPlaceService(PlaceService placeService) {
+		this.placeService = placeService;
+	}
+}
