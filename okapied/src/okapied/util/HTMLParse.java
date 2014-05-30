41448b858fb62c15a0cb3ec9e2cb49d464769b02
edenduthie <eduthie@gmail.com>
2014-05-30 03:56:36 +0000

    HTMLParse.java

---
+++ okapied/src/okapied/util/HTMLParse.java
@@ -0,0 +1,16 @@
+package okapied.util;
+
+import javax.swing.text.html.HTMLEditorKit;
+
+public class HTMLParse extends HTMLEditorKit
+{
+  /**
+   * Call to obtain a HTMLEditorKit.Parser object.
+   * 
+   * @return A new HTMLEditorKit.Parser object.
+   */
+  public HTMLEditorKit.Parser getParser()
+  {
+    return super.getParser();
+  }
+}
