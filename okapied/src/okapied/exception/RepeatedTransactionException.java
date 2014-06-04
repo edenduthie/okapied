e2c5f3cdd79aeeaef21b2bfbe6ccfe4fe980191f
edenduthie <eduthie@gmail.com>
2014-06-04 03:58:47 +0000

    RepeatedTransactionException.java

---
+++ okapied/src/okapied/exception/RepeatedTransactionException.java
@@ -0,0 +1,24 @@
+package okapied.exception;
+
+public class RepeatedTransactionException extends Exception {
+
+	public RepeatedTransactionException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public RepeatedTransactionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RepeatedTransactionException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RepeatedTransactionException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
