637f7477aeecb353e6c9a517becfc13adbd9e403
edenduthie <eduthie@gmail.com>
2014-06-04 03:56:20 +0000

    RefundException.java

---
+++ okapied/src/okapied/exception/RefundException.java
@@ -0,0 +1,24 @@
+package okapied.exception;
+
+public class RefundException extends Exception {
+
+	public RefundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public RefundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RefundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RefundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
