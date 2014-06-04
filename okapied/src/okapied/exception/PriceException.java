0268f1a4520fc26d54cb2ea5c3ffb46f068eb3ea
edenduthie <eduthie@gmail.com>
2014-06-04 03:56:00 +0000

    PriceException.java

---
+++ okapied/src/okapied/exception/PriceException.java
@@ -0,0 +1,24 @@
+package okapied.exception;
+
+public class PriceException extends Exception {
+
+	public PriceException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PriceException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PriceException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PriceException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
