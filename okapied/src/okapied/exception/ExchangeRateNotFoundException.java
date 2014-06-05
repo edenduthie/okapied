a7466b3cce60aad252a8311afdb3d40aeede1a73
edenduthie <eduthie@gmail.com>
2014-06-05 03:57:03 +0000

    ExchangeRateNotFoundException.java

---
+++ okapied/src/okapied/exception/ExchangeRateNotFoundException.java
@@ -0,0 +1,24 @@
+package okapied.exception;
+
+public class ExchangeRateNotFoundException extends Exception {
+
+	public ExchangeRateNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExchangeRateNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExchangeRateNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExchangeRateNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
