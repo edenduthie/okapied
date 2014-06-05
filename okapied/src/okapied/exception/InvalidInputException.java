429c9a4f8082385a935ae86b5c51233daeee6776
edenduthie <eduthie@gmail.com>
2014-06-05 03:59:34 +0000

    InvalidInputException.java

---
+++ okapied/src/okapied/exception/InvalidInputException.java
@@ -0,0 +1,24 @@
+package okapied.exception;
+
+public class InvalidInputException extends Exception {
+
+	public InvalidInputException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidInputException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidInputException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidInputException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
