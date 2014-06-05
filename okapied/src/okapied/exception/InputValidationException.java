afc52df0e552f87ea9b2290b0ba84d0972a3c856
edenduthie <eduthie@gmail.com>
2014-06-05 03:56:22 +0000

    InputValidationException.java

---
+++ okapied/src/okapied/exception/InputValidationException.java
@@ -0,0 +1,24 @@
+package okapied.exception;
+
+public class InputValidationException extends Exception {
+
+	public InputValidationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InputValidationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InputValidationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InputValidationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
