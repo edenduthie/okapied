8e71a3afbfa55c90f0aa88bf6a4ddfd3d92ac729
edenduthie <eduthie@gmail.com>
2014-06-05 03:57:54 +0000

    PaymentException.java

---
+++ okapied/src/okapied/exception/PaymentException.java
@@ -0,0 +1,24 @@
+package okapied.exception;
+
+public class PaymentException extends Exception {
+
+	public PaymentException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PaymentException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PaymentException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PaymentException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
