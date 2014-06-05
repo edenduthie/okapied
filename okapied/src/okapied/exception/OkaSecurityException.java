0ceb5ac384334e4971fd8c0f40329b479355ba70
edenduthie <eduthie@gmail.com>
2014-06-05 03:55:57 +0000

    OkaSecurityException.java

---
+++ okapied/src/okapied/exception/OkaSecurityException.java
@@ -0,0 +1,24 @@
+package okapied.exception;
+
+public class OkaSecurityException extends Exception {
+
+	public OkaSecurityException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public OkaSecurityException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OkaSecurityException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OkaSecurityException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
