18a61bc3552f1a7ff8fab3ab4dd90ea29d471eb4
edenduthie <eduthie@gmail.com>
2014-06-05 03:58:57 +0000

    FeedbackException.java

---
+++ okapied/src/okapied/exception/FeedbackException.java
@@ -0,0 +1,26 @@
+package okapied.exception;
+
+public class FeedbackException extends Exception
+{
+
+	public FeedbackException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FeedbackException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FeedbackException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FeedbackException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
