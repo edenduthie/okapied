997884a3497ec8a55ad55ff5734d49c09119b591
edenduthie <eduthie@gmail.com>
2014-06-05 03:55:44 +0000

    InvalidPayPalAccountException.java

---
+++ okapied/src/okapied/exception/InvalidPayPalAccountException.java
@@ -0,0 +1,24 @@
+package okapied.exception;
+
+public class InvalidPayPalAccountException extends Exception {
+
+	public InvalidPayPalAccountException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidPayPalAccountException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidPayPalAccountException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidPayPalAccountException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
