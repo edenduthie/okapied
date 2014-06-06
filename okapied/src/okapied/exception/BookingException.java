a6533bb48f43d9b80825b743fa3fefb2311c6061
edenduthie <eduthie@gmail.com>
2014-06-06 03:58:01 +0000

    BookingException.java

---
+++ okapied/src/okapied/exception/BookingException.java
@@ -0,0 +1,24 @@
+package okapied.exception;
+
+public class BookingException extends Exception {
+
+	public BookingException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BookingException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BookingException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BookingException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
