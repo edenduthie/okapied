d7f4248dde6336d090a397f2fe08cfbcff800717
edenduthie <eduthie@gmail.com>
2014-06-06 03:57:34 +0000

    CancellationException.java

---
+++ okapied/src/okapied/exception/CancellationException.java
@@ -0,0 +1,25 @@
+package okapied.exception;
+
+public class CancellationException extends Exception {
+
+	public CancellationException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CancellationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CancellationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CancellationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
