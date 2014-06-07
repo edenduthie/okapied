8e8601e6e6772fd45fcd0643f6db0544f47bb5eb
edenduthie <eduthie@gmail.com>
2014-06-07 03:57:16 +0000

    AvailabilityException.java

---
+++ okapied/src/okapied/exception/AvailabilityException.java
@@ -0,0 +1,24 @@
+package okapied.exception;
+
+public class AvailabilityException extends Exception {
+
+	public AvailabilityException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AvailabilityException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AvailabilityException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AvailabilityException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
