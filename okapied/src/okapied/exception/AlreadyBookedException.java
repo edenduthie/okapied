1227616335ace1be703f1a076da4bd1a680fa680
edenduthie <eduthie@gmail.com>
2014-06-07 03:55:15 +0000

    AlreadyBookedException.java

---
+++ okapied/src/okapied/exception/AlreadyBookedException.java
@@ -0,0 +1,24 @@
+package okapied.exception;
+
+public class AlreadyBookedException extends Exception {
+
+	public AlreadyBookedException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AlreadyBookedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AlreadyBookedException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AlreadyBookedException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
